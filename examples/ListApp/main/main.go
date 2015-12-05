5891eff31e6d15636da77d4fce654828f66e16e2
Dave MacFarlane <driusan@gmail.com>
2015-12-05 12:29:27 -0500

    Initial SQLIte List app

---
+++ examples/ListApp/main/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"URLHandler"
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"net/http"
+)
+
+type ListPage struct {
+	URLHandler.DefaultHandler
+}
+
+func (r ListPage) Get(req *http.Request, extraparams map[string]interface{}) (string, error) {
+	db := extraparams["database"].(*sql.DB)
+	template := "<html><body>"
+	template += "<h3>" + extraparams["title"].(string) + "</h3>"
+	template += "	<form method=\"post\"><ul>"
+
+	rows, err := db.Query("SELECT Value FROM listitems")
+	defer rows.Close()
+	if err != nil {
+		return "Error Querying Database", err
+	}
+
+	for rows.Next() {
+		var value string
+		rows.Scan(&value)
+		template += "<li>" + value + "</li>"
+	}
+	template += "</ul><input type=\"text\" name=\"input\"/><input type=\"submit\" /></form></body></html>"
+
+	return template, nil
+}
+
+func (r ListPage) Post(req *http.Request, extraparams map[string]interface{}) (string, string, error) {
+	var db = extraparams["database"].(*sql.DB)
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(req.PostForm.Get("input"))
+	insert, err := db.Prepare("INSERT INTO listitems (Value) VALUES (?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = insert.Exec(req.PostForm.Get("input"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return "Data has been inserted", "/", nil
+}
+
+func main() {
+	x := ListPage{}
+	db, err := sql.Open("sqlite3", "list.db")
+	defer db.Close()
+	if err != nil {
+		panic("Couldn't open db")
+	}
+
+	URLHandler.RegisterExtraParameter("title", "I am a list")
+	URLHandler.RegisterExtraParameter("database", db)
+	URLHandler.RegisterHandler(x, "/")
+	fmt.Printf("Listening and serving on port 8080\n")
+	http.ListenAndServe(":8080", nil)
+}
